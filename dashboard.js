399a30d71d4285698ed29585f2a209875f1f5b0f
seniorcub <farinderif@gmail.com>
2024-07-08 16:18:04 +0100

    refactor: Improve dashboard.js functionality and optimize code

--- dashboard.js
+++ dashboard.js
@@ -1,62 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-     const allLinks = document.querySelectorAll(".sidebar-links a");
-     const sections = document.querySelectorAll(".section");
-
-     // Initially hide all sections
-     sections.forEach((section) => {
-          section.style.display = "none";
-     });
-
-     allLinks.forEach((link) => {
-          link.addEventListener("click", function (event) {
-               event.preventDefault();
-               const targetId = link.getAttribute('href').substring(1); // Get the target ID without the '#'
-
-               // Hide all sections
-               sections.forEach((section) => {
-                    section.style.display = "none";
-               });
-
-               // Show the target section
-               document.getElementById(targetId).style.display = "block";
-
-               // Remove 'active' class from all links
-               allLinks.forEach((link) => {
-                    link.classList.remove("active");
-               });
-
-               // Add 'active' class to the clicked link
-               link.classList.add("active");
-          });
-     });
-
-     // Optionally, show the first section by default
-     if (sections.length > 0) {
-          sections[0].style.display = "block";
-          allLinks[1].classList.add("active");
-     }
-});
-
-let label = document.querySelector("#upload");
-let icon = document.querySelector(".icon");
-let input = document.querySelector("#file");
-label.addEventListener("click",()=>{
-     input.click();
-})
-icon.addEventListener("click",()=>{
-     input.click();
-})
-document.querySelector(".service").addEventListener("click",()=>{
-     window.location="/services.html"
-})
-document.querySelector("#addOrder").addEventListener("click",()=>{
-     window.location="/services.html"
-})
-document.querySelectorAll(".home").forEach((ele)=>{
-     ele.addEventListener("click",()=>{
-          window.location="/"
-     });
-})
 document.querySelector("#refresh").addEventListener("click",()=>{
      window.location.reload();
      // Refresh API
@@ -64,14 +5,6 @@ document.querySelector("#refresh").addEventListener("click",()=>{
 document.querySelector("#refreshOrder").addEventListener("click",()=>{
      window.location.reload();
      // Refresh API
-});
-document.querySelector("#add").addEventListener("click",()=>{
-     document.querySelector(".section").style.display="none";
-     document.querySelector("#transactions").style.display="none";
-     document.querySelector("#deposit").style.display="block";
-     // Remove active from transaction and add to deposit link
-     document.querySelector(".transaction").classList.remove("active");
-     document.querySelector(".deposit").classList.add("active");
 });
 document.querySelector("#save").addEventListener("click",()=>{
      document.querySelector("#alerting").innerHTML=`
